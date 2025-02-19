713032d9a02fe556a9ef9793c727e83af649436d
COMLAB <>
2025-02-19 18:40:10 +0800

    UPDATED JS/PHP
    sdasdasd

--- index.php
+++ index.php
@@ -10,11 +10,12 @@
 <head>
     <style>
     fieldset {
-    background-color: #eeeeee;
+    background-color: papayawhip;
     }
 
     legend {
-    background-color: gray;
+    
+    background-color: gray; 
     color: white;
     padding: 5px 10px;
     }
@@ -26,19 +27,20 @@
 </head>
     <body>
 
-    <form action="/action_page.php">
+    <form action="/action_page.php" method="POST">
+        
     <fieldset>
-        <legend>Form:</legend>
-        <label for="fname">First name:</label>
-        <input type="text" id="fname" name="fname"><br><br>
-        <label for="lname">Last name:</label>
-        <input type="text" id="lname" name="lname"><br><br>
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email"><br><br>
-        <label for="birthday">Birthday:</label>
-        <input type="date" id="birthday" name="birthday"><br><br>
-        <input type="submit" value="Submit">
-    </fieldset>
+            <legend>Form:</legend>
+            <label for="fname">First name:</label>
+            <input type="text" id="fname" name="fname"><br><br>
+            <label for="lname">Last name:</label>
+            <input type="text" id="lname" name="lname"><br><br>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email"><br><br>
+            <label for="birthday">Birthday:</label>
+            <input type="date" id="birthday" name="birthday"><br><br>
+            <input type="submit" value="Save Record" name="submit">
+        </fieldset>
     </form>
 
     </body>
