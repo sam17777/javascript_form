d8bf4af7846a8a066ba286dfc678a5d4d19805be
COMLAB <>
2025-02-19 19:45:29 +0800

    UPD TABLE

--- table.php
+++ table.php
@@ -7,8 +7,50 @@
 </head>
 <body>
     <h1>Student Record</h1>
-    <table id="student record">
-    
+    <table id="student_record" border="1">
+        
     </table>
+
+    <script>
+        const studentRecord = document.getElementById("student_record");
+   
+        console.log("Hello World");
+        const row1 = studentRecord.insertRow();
+
+        const row1col1 = row1.insertCell();
+        row1col1.innerHTML= "Last Name";
+
+        const row1col2 = row1.insertCell();
+        row1col2.innerHTML= "First Name";
+
+        const row2 = studentRecord.insertRow();
+        const row2col1 = row2.insertCell();
+        const row2col2 = row2.insertCell();
+    
+        const inputLastName =document.createElement("INPUT");
+        inputLastName.type = "text";
+        inputLastName.placeholder = "Enter Last Name";
+
+        const inputFirstName =document.createElement("INPUT");
+        inputFirstName.type = "text";
+        inputFirstName.placeholder = "Enter First Name";
+
+        const row3 = studentRecord.insertRow();
+        const row3col1 = row3.insertCell().colSpan = "2";
+        const submit =document.createElement("INPUT");
+        submit.type = "submit";
+        submit.placeholder = "SUBMIT";
+    
+
+
+
+
+  
+        row3.append(submit);
+        row2col1.append(inputLastName);
+        row2col2.append(inputFirstName);
+
+        console.log(studentRecord);
+    </script>
 </body>
 </html>
